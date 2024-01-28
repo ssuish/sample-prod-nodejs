91a88b7ef69c2dde1dfcf6f975fc44d49f82462f
suisoh <88411618+ssuish@users.noreply.github.com>
2024-01-28 13:26:50 +0800

    Update index.js with HTTP server implementation

--- index.js
+++ index.js
@@ -1,33 +1,11 @@
-const { MongoClient } = require("mongodb");
-
-async function main() {
-  /**
-   * Connection URI. Update <your-mongo-connection-string> to reflect your cluster. You can find it on the
-   * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
-   */
-
-  const uri = "mongodb+srv://ssuish:KcIEjoYUBynY2SVR@nodejs-cluster0.vpkfdv8.mongodb.net/?retryWrites=true&w=majority";
-
-  const client = new MongoClient(uri);
-
-  try {
-    // Connect to the MongoDB cluster
-    await client.connect();
-
-    // Make the appropriate DB calls
-    await listDatabases(client);
-  } catch (e) {
-    console.error(e);
-  } finally {
-    await client.close();
-  }
-}
-
-async function listDatabases(client) {
-  databasesList = await client.db().admin().listDatabases();
-
-  console.log("Connected successfully. Databases:");
-  databasesList.databases.forEach((db) => console.log(` - ${db.name}`));
-}
-
-main().catch(console.error);
+const http = require('node:http');
+const hostname = '127.0.0.1';
+const port = 3000;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
